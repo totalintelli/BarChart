96bcaa49db1b02b5f37e98461f2f61249944f3f6
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-20 18:04:16 +0900

    first commit

---
+++ BarChart/FrmBarChart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarChart
+{
+    public partial class FrmBarChart : Form
+    {
+        public FrmBarChart()
+        {
+            InitializeComponent();
+        }
+
+        // TransForm the Graphics object so
+        // world coordinates wxmin <= X <= wxmax, wymin <= Y <= wymax are mapped to
+        // 
+    }
+}
